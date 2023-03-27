d9e6e26e166d5239eff78595b4f90fbd7311100d
Shivani610 <rautshivani1806@gmail.com>
2023-03-27 11:46:50 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose")
 const db = () => {
     try {
         mongoose.set('strictQuery', true);
-        // 2eRi1Xg7GpLUuYdP password
-        // mongodb+srv://shivani:2eRi1Xg7GpLUuYdP@cluster0.lh3b6zn.mongodb.net/?   retryWrites=true&w=majority
+
         mongoose.connect(process.env.MONGO_URL)
     } catch (error) {
         console.log(`Unable to Connected ${error}`);
